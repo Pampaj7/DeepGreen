fd745e965dd598b0a60788ddb3b6df3385b903d8
marcopaglio <marco.paglio1997@gmail.com>
2025-06-11 11:57:30 +0200

    Refactored Python scripts

--- cpp/py_script/resnet18.py
+++ cpp/py_script/resnet18.py
@@ -2,31 +2,29 @@ import torch
 from torchvision import models
 import sys
 
-def export_resnet18(output_name = "resnet18", num_classes: int = 200, pretrained_weights = None):
-    for model, name in (
-            (models.resnet18(weights=pretrained_weights), output_name),
-    ):
-        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+def export_resnet18(output_name = "resnet18", num_classes: int = 100, pretrained_weights = None):
+    model = models.resnet18(weights=pretrained_weights)
+    model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
 
-        if not pretrained_weights:
-            def init_weights(m):
-                if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
-                    torch.nn.init.kaiming_normal_(m.weight)
-                    if m.bias is not None:
-                        torch.nn.init.zeros_(m.bias)
+    if not pretrained_weights:
+        def init_weights(m):
+            if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
+                torch.nn.init.kaiming_normal_(m.weight)
+                if m.bias is not None:
+                    torch.nn.init.zeros_(m.bias)
 
-            model.apply(init_weights)
+        model.apply(init_weights)
 
-        traced_model = torch.jit.script(model)
-        traced_model.save(f"{name}.pt")
+    traced_model = torch.jit.script(model)
+    traced_model.save(f"{output_name}.pt")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        params = {}
-    elif len(sys.argv) == 2:
-        params = {"output_name": sys.argv[1]}
-    else: # len(sys.argv) == 3 or more
-        params = {"output_name": sys.argv[1], "num_classes": int(sys.argv[2])}
+    params = {}
+    if len(sys.argv) > 1:
+        params["output_name"] = sys.argv[1]
+    if len(sys.argv) > 2:
+        params["num_classes"] = int(sys.argv[2])
+    #params["pretrained_weights"] = weights=models.ResNet18_Weights.IMAGENET1K_V1
 
-    export_resnet18(**(params)) # weights=models.ResNet18_Weights.IMAGENET1K_V1 #
+    export_resnet18(**(params))
