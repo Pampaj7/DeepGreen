897dc067897484479907bbb51a8d0afb3b06b09a
Pampaj7 <leonardo.pampaloni29@gmail.com>
2025-07-16 10:46:57 +0000

    updated code but missing compatible version of server

---
+++ R/train/resnet18/train_cifar100.r
@@ -0,0 +1,8 @@
+source("R/models/resnet18.r")
+cat("Script avviato!\n")
+run_experiment(
+  dataset_path = "R/data/cifar100_png",
+  checkpoint_path = "R/checkpoints/resnet18_cifar100_r.pt",
+  img_size = c(32, 32),
+  grayscale = FALSE
+)
