0dc83c26bf06515295baff5f7b0815efc766e768
marcopaglio <marco.paglio1997@gmail.com>
2025-08-01 15:05:42 +0200

    vgg16 on tiny imagenet

---
+++ Java/DL4J/deepgreen-dl4j-expt/src/main/java/io/github/stlabunifi/deepgreen/dl4j/expt/vgg16/ModelInspector.java
@@ -0,0 +1,49 @@
+package io.github.stlabunifi.deepgreen.dl4j.expt.vgg16;
+
+import java.util.List;
+import java.util.Map;
+
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.conf.graph.GraphVertex;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+
+public class ModelInspector {
+
+	public static void printGraphDetails(ComputationGraph graph) {
+		ComputationGraphConfiguration conf = graph.getConfiguration();
+
+		System.out.println("===== COMPUTATION GRAPH DETAILS =====\n");
+
+		// INPUTS
+		System.out.println("Inputs:");
+		for (String input : conf.getNetworkInputs()) {
+			System.out.println(" - " + input);
+		}
+
+		// OUTPUTS
+		System.out.println("\nOutputs:");
+		for (String output : conf.getNetworkOutputs()) {
+			System.out.println(" - " + output);
+		}
+
+		// VERTICES
+		System.out.println("\nVertices:");
+		for (Map.Entry<String, GraphVertex> entry : conf.getVertices().entrySet()) {
+			String name = entry.getKey();
+			GraphVertex vertex = entry.getValue();
+			System.out.println(" - Name: " + name);
+			System.out.println("   Class: " + vertex.getClass().getSimpleName());
+			System.out.println("   Vertex: " + vertex.toString());
+			List<String> inputsToVertex = conf.getVertexInputs().get(name);
+			if (inputsToVertex != null && inputsToVertex.size() > 0) {
+				System.out.print("   Inputs: ");
+				for (String in : inputsToVertex) {
+					System.out.print(in + " ");
+				}
+				System.out.println();
+			}
+		}
+
+		System.out.println("\n======================================");
+	}
+};
---
+++ Java/DL4J/deepgreen-dl4j-expt/src/main/java/io/github/stlabunifi/deepgreen/dl4j/expt/vgg16/VGG16TinyImageNetRebuilder.java
@@ -0,0 +1,70 @@
+package io.github.stlabunifi.deepgreen.dl4j.expt.vgg16;
+
+import java.util.List;
+
+import org.deeplearning4j.nn.conf.CNN2DFormat;
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.conf.ConvolutionMode;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.nd4j.linalg.learning.config.Adam;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+
+public class VGG16TinyImageNetRebuilder {
+
+	public static final int imgHeight = 64;
+	public static final int imgWidth = 64;
+	public static final int imgChannels = 3;
+	public static final int numClasses = 200;
+
+	public static ComputationGraph rebuildModelWithInputShape(ComputationGraph importedModel, long seed) {
+		// Get imported model configuration
+		ComputationGraphConfiguration importedConf = importedModel.getConfiguration();
+
+		// Create same configuration with input shape
+		ComputationGraphConfiguration.GraphBuilder graphBuilder = new NeuralNetConfiguration.Builder()
+				.seed(seed)
+				.updater(new Adam(1e-5))
+				.weightInit(WeightInit.RELU)
+				.activation(Activation.IDENTITY)
+				.convolutionMode(ConvolutionMode.Same)
+				.graphBuilder()
+					.addInputs(importedConf.getNetworkInputs().get(0))
+					.setInputTypes(InputType.convolutional(
+							imgHeight,
+							imgWidth,
+							imgChannels,
+							CNN2DFormat.NCHW));
+		
+		for (String vertexName : importedConf.getVertices().keySet()) {
+			List<String> inputs = importedConf.getVertexInputs().get(vertexName);
+			
+			String[] stringInputs = new String[inputs.size()];
+			for (int i = 0; i < inputs.size(); i++) {
+				stringInputs[i] = inputs.get(i);
+				
+			}
+			graphBuilder.addVertex(
+					vertexName, 
+					importedConf.getVertices().get(vertexName),
+					stringInputs);
+		}
+
+		ComputationGraphConfiguration newConf = graphBuilder
+				.setOutputs(importedConf.getNetworkOutputs().get(0))
+				.build();
+
+		// Create a new model
+		ComputationGraph newModel = new ComputationGraph(newConf);
+		newModel.init();
+		
+		//newModel.setParams(importedModel.params());
+		
+		ModelInspector.printGraphDetails(newModel);
+
+		return newModel;
+	}
+
+}
