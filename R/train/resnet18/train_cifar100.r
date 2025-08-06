f31f092262f84676986d3c93131a99d2688bd75d
Pampaj7 <leonardo.pampaloni29@gmail.com>
2025-08-06 15:48:49 +0000

    R working on the server

--- R/train/resnet18/train_cifar100.r
+++ R/train/resnet18/train_cifar100.r
@@ -1,7 +1,7 @@
 source("R/models/resnet18.r")
 cat("Script avviato!\n")
 run_experiment(
-  dataset_path = "R/data/cifar100_png",
+  dataset_path = "/home/pampaj/DeepGreen/data/cifar100_png",
   checkpoint_path = "R/checkpoints/resnet18_cifar100_r.pt",
   img_size = c(32, 32),
   grayscale = FALSE
