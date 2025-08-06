b159f0ed02d4c1a804a83c53e855249b49c5551a
marcopaglio <marco.paglio1997@gmail.com>
2025-08-06 17:43:41 +0200

    Using native VGG16

--- Java/DL4J/deepgreen-dl4j-expt/src/main/java/io/github/stlabunifi/deepgreen/dl4j/expt/vgg16/Vgg16TrainCifar100Expt.java
+++ Java/DL4J/deepgreen-dl4j-expt/src/main/java/io/github/stlabunifi/deepgreen/dl4j/expt/vgg16/Vgg16TrainCifar100Expt.java
@@ -5,21 +5,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.nd4j.common.io.ClassPathResource;
-import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+//import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+//import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.VGG16ImagePreProcessor;
 
 import io.github.stlabunifi.deepgreen.dl4j.core.dataloader.Cifar100Dataloader;
-import io.github.stlabunifi.deepgreen.dl4j.core.model.ModelRebuilder;
+//import io.github.stlabunifi.deepgreen.dl4j.core.model.ModelRebuilder;
+import io.github.stlabunifi.deepgreen.dl4j.core.model.VGG16NativeModel;
 import io.github.stlabunifi.deepgreen.dl4j.python.handler.PythonCommandHandler;
 
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 
 public class Vgg16TrainCifar100Expt {
 
-	public final static String vgg16_py_filepath = "/models/sequential_vgg16.py";
-	public final static String vgg16_cifar100_h5_filename = "sequential_vgg16_cifar100.h5";
+	//public final static String vgg16_py_filepath = "/models/sequential_vgg16.py";
+	//public final static String vgg16_cifar100_h5_filename = "sequential_vgg16_cifar100.h5";
 
 	public final static int rngSeed = 12345; 	// random number seed for reproducibility
 	public final static int batchSize = 128; 	// batch size for each epoch
@@ -37,12 +39,12 @@ public class Vgg16TrainCifar100Expt {
 	public static void main(String[] args) throws Exception {
 		try {
 			// Generate Keras model
-			Path modelFilePath = Paths.get(vgg16_cifar100_h5_filename);
-			if (!Files.exists(modelFilePath) || !Files.isRegularFile(modelFilePath)) {
-				System.out.println("Generating VGG-16 model in h5 format...");
-				String pyScriptFullPath = new ClassPathResource(vgg16_py_filepath).getFile().getPath();
-				PythonCommandHandler.runGenerateModelScript(pyScriptFullPath, vgg16_cifar100_h5_filename, numClasses, lrAdam);
-			}
+			//Path modelFilePath = Paths.get(vgg16_cifar100_h5_filename);
+			//if (!Files.exists(modelFilePath) || !Files.isRegularFile(modelFilePath)) {
+			//	System.out.println("Generating VGG-16 model in h5 format...");
+			//	String pyScriptFullPath = new ClassPathResource(vgg16_py_filepath).getFile().getPath();
+			//	PythonCommandHandler.runGenerateModelScript(pyScriptFullPath, vgg16_cifar100_h5_filename, numClasses, lrAdam);
+			//}
 
 			// Load CIFAR-100
 			Path datasetDir = Paths.get(cifar100_png_dirpath);
@@ -61,12 +63,15 @@ public class Vgg16TrainCifar100Expt {
 
 
 			// Import Keras VGG-16 model with training config
-			MultiLayerNetwork importedVgg16 = KerasModelImport.importKerasSequentialModelAndWeights(
-					/* modelHdf5Stream = */vgg16_cifar100_h5_filename,
-					/* enforceTrainingConfig = */true);
-			
-			MultiLayerNetwork vgg16 = ModelRebuilder
-					.rebuildSequentialModelWithOutputLayer(importedVgg16, rngSeed, imgHeight, imgWidth, imgChannels);
+			//MultiLayerNetwork importedVgg16 = KerasModelImport.importKerasSequentialModelAndWeights(
+			//		/* modelHdf5Stream = */vgg16_cifar100_h5_filename,
+			//		/* enforceTrainingConfig = */true);
+			//
+			//MultiLayerNetwork vgg16 = ModelRebuilder
+			//		.rebuildSequentialModelWithOutputLayer(importedVgg16, rngSeed, imgHeight, imgWidth, imgChannels);
+
+			ComputationGraph vgg16 = VGG16NativeModel.buildVGG16(numClasses, rngSeed, 
+					new int[] {imgChannels, imgHeight, imgWidth}, lrAdam);
 
 
 			// Listener
