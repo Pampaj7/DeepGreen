e207f6204852f4b3e5ca7c8c40a1d47d5403503c
marcopaglio <marco.paglio1997@gmail.com>
2025-08-08 18:59:04 +0200

    Added tracking

--- Java/DL4J/deepgreen-dl4j-expt/src/main/java/io/github/stlabunifi/deepgreen/dl4j/expt/vgg16/Vgg16TrainTinyExpt.java
+++ Java/DL4J/deepgreen-dl4j-expt/src/main/java/io/github/stlabunifi/deepgreen/dl4j/expt/vgg16/Vgg16TrainTinyExpt.java
@@ -13,19 +13,22 @@ import org.nd4j.linalg.dataset.api.preprocessor.VGG16ImagePreProcessor;
 import io.github.stlabunifi.deepgreen.dl4j.core.dataloader.TinyImageNetDataloader;
 import io.github.stlabunifi.deepgreen.dl4j.core.model.builder.Vgg16GraphBuilder;
 import io.github.stlabunifi.deepgreen.dl4j.python.handler.PythonCommandHandler;
-
+import io.github.stlabunifi.deepgreen.dl4j.python.handler.PythonTrackerHandler;
 
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 
 public class Vgg16TrainTinyExpt {
 
+	public final static String emission_output_dir = "emissions";
+	public final static String emission_filename = "vgg16_tiny.csv";
+
 	//public final static String vgg16_py_filepath = "/models/vgg16.py";
 	//public final static String vgg16_tiny_h5_filename = "vgg16_tiny.h5";
 
 	public final static int rngSeed = 1234; 	// random number seed for reproducibility
-	public final static int batchSize = 64; 	// batch size for each epoch
+	public final static int batchSize = 128; 	// batch size for each epoch
 	public final static int numClasses = 200; 	// number of output classes
-	public final static int numEpochs = 30;		// number of epochs to perform
+	public final static int numEpochs = 1;		// number of epochs to perform
 	public final static double lrAdam = 1e-5;	// learning rate used in Adam optimizer
 
 	public static final int transformed_imgHeight = 32;
@@ -35,8 +38,16 @@ public class Vgg16TrainTinyExpt {
 	public static final String tiny_downloader_py_filepath = "/dataset/download_convert_tinyimage.py"; // located in reasources
 	public static final String tiny_png_dirpath = "data/tiny_imagenet_png";
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		try {
+			Path emissionOutputDir = Paths.get(emission_output_dir).toAbsolutePath();
+			// Remove existing emission file
+			Path emissionFilePath = emissionOutputDir.resolve(emission_filename);
+			if (Files.exists(emissionFilePath) && !Files.isDirectory(emissionFilePath))
+				Files.delete(emissionFilePath);
+
+			PythonTrackerHandler trackerHandler = new PythonTrackerHandler(emissionOutputDir.toString());
+
 			// Generate Keras model
 			//Path modelFilePath = Paths.get(vgg16_tiny_h5_filename);
 			//if (!Files.exists(modelFilePath) || !Files.isRegularFile(modelFilePath)) {
@@ -81,13 +92,17 @@ public class Vgg16TrainTinyExpt {
 			// Training
 			System.out.println("Starting training...");
 			for (int i = 0; i < numEpochs; i++) {
+				trackerHandler.startTracker(emission_filename);
 				vgg16.fit(tinyTrain);
+				trackerHandler.stopTracker();
 				System.out.println("Epoch " + (i + 1) + " completed.");
 			}
 			
 			// Evaluation
 			System.out.println("Starting evaluation...");
+			trackerHandler.startTracker(emission_filename);
 			var eval = vgg16.evaluate(tinyTest);
+			trackerHandler.stopTracker();
 			System.out.println(eval.stats());
 			
 		} catch (Exception e) {
