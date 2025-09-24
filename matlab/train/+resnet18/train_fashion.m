306b8caef0726f5880d07f51410bcaf591c6046c
marcopaglio <marco.paglio1997@gmail.com>
2025-09-24 17:55:10 +0200

    Matlab: using emissionFileName as param with default value

--- matlab/train/+resnet18/train_fashion.m
+++ matlab/train/+resnet18/train_fashion.m
@@ -6,19 +6,19 @@
 % 2) Before run, set the matlab folder (and subdirectories) to matlab's paths via:
 % >> addpath(genpath('matlab'));
 % 3) Run the function via (don't change location)
-% >> resnet18.train_fashion('data/fashion_mnist_png','matlab/checkpoints/resnet18_fashion_matlab.mat',30,128);
+% >> resnet18.train_fashion('data/fashion_mnist_png','resnet18_fashion','matlab/checkpoints/resnet18_fashion_matlab.mat',30,128);
 % 
 % Alternatvely, run :
-% $ matlab -batch "; resnet18.train_fashion('data/fashion_mnist_png','matlab/checkpoints/resnet18_fashion_matlab.mat',30,128); exit"
+% $ matlab -batch "; resnet18.train_fashion('data/fashion_mnist_png','resnet18_fashion','matlab/checkpoints/resnet18_fashion_matlab.mat',30,128); exit"
 %
-function train_fashion(datasetDir, outMat, epochs, batchSize)
+function train_fashion(datasetDir, emissionFileName, outMat, epochs, batchSize)
     % --------- default args ---------
-    if nargin<1||isempty(datasetDir), datasetDir = 'data/fashion_mnist_png'; end
-    if nargin<2||isempty(outMat),     outMat     = 'matlab/checkpoints/resnet18_fashion_matlab.mat'; end
-    if nargin<3||isempty(epochs),     epochs     = 30; end
-    if nargin<4||isempty(batchSize),  batchSize  = 128; end
+    if nargin<1||isempty(datasetDir),       datasetDir          = 'data/fashion_mnist_png'; end
+    if nargin<2||isempty(emissionFileName), emissionFileName    = 'resnet18_fashion'; end
+    if nargin<3||isempty(outMat),           outMat              = 'matlab/checkpoints/resnet18_fashion_matlab.mat'; end
+    if nargin<4||isempty(epochs),           epochs              = 30; end
+    if nargin<5||isempty(batchSize),        batchSize           = 128; end
     emissionOutputDir = 'matlab/emissions';
-    emissionFileName = 'resnet18_fashion';
 
     % --------- DATA ---------
     trainDir = fullfile(datasetDir,'train');
