0d23eb3f65047a2d2182b7df0c968e996db0268c
marcopaglio <marco.paglio1997@gmail.com>
2025-09-26 16:58:05 +0200

    Updated MATLAB scripts

--- matlab/run_scripts_on_unix.sh
+++ matlab/run_scripts_on_unix.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # === Sequential execution of each MATLAB training script ===
 
-matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); resnet18.train_cifar100; exit"
+/usr/local/MATLAB/R2023a/bin/matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); resnet18.train_cifar100; exit" -sd '/home/marcopaglio/DeepGreen/matlab'
 sleep 30
 
-matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); resnet18.train_fashion; exit"
+/usr/local/MATLAB/R2023a/bin/matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); resnet18.train_fashion; exit" -sd '/home/marcopaglio/DeepGreen/matlab'
 sleep 30
 
-matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); resnet18.train_tiny; exit"
+/usr/local/MATLAB/R2023a/bin/matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); resnet18.train_tiny; exit" -sd '/home/marcopaglio/DeepGreen/matlab'
 sleep 30
 
-matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); vgg16.train_cifar100; exit"
+/usr/local/MATLAB/R2023a/bin/matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); vgg16.train_cifar100; exit" -sd '/home/marcopaglio/DeepGreen/matlab'
 sleep 30
 
-matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); vgg16.train_fashion; exit"
+/usr/local/MATLAB/R2023a/bin/matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); vgg16.train_fashion; exit" -sd '/home/marcopaglio/DeepGreen/matlab'
 sleep 30
 
-matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); vgg16.train_tiny; exit"
+/usr/local/MATLAB/R2023a/bin/matlab -batch "setup_env('/usr/bin/python3', '/home/marcopaglio/DeepGreen'); vgg16.train_tiny; exit" -sd '/home/marcopaglio/DeepGreen/matlab'
 
 echo "All scripts have been executed."
--- matlab/run_scripts_on_windows.bat
+++ matlab/run_scripts_on_windows.bat
@@ -1,22 +1,22 @@
 @echo off
 REM === Sequential execution of each MATLAB training script ===
 
-matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); resnet18.train_cifar100; exit"
+matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); resnet18.train_cifar100; exit" -sd 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen\matlab'
 timeout /t 30 /nobreak
 
-matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); resnet18.train_fashion; exit"
+matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); resnet18.train_fashion; exit" -sd 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen\matlab'
 timeout /t 30 /nobreak
 
-matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); resnet18.train_tiny; exit"
+matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); resnet18.train_tiny; exit" -sd 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen\matlab'
 timeout /t 30 /nobreak
 
-matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); vgg16.train_cifar100; exit"
+matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); vgg16.train_cifar100; exit" -sd 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen\matlab'
 timeout /t 30 /nobreak
 
-matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); vgg16.train_fashion; exit"
+matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); vgg16.train_fashion; exit" -sd 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen\matlab'
 timeout /t 30 /nobreak
 
-matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); vgg16.train_tiny; exit"
+matlab -batch "setup_env('C:\Users\marco_u3rv1hf\AppData\Local\Programs\Python\Python39\python', 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen'); vgg16.train_tiny; exit" -sd 'C:\Users\marco_u3rv1hf\Desktop\Unifi\Borse_di_ricerca\00_AImetrics_Verdecchia\Progetto\DeepGreen\matlab'
 
 echo All scripts have been executed.
 pause
